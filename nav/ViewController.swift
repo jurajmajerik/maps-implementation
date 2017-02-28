13dab2d39bc08498b2e5164985735fb78f3b5819
jurajmajerik <juro.majerik@gmail.com>
2017-02-28 23:09:42 +0100

    first commit

--- nav/ViewController.swift
+++ nav/ViewController.swift
@@ -7,8 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func showMeWhere(_ sender: Any)
+    {
+        // Define destination
+        let latitude: CLLocationDegrees = 52.365467
+        let longitude: CLLocationDegrees = 4.898581
+        
+        let regionDistance: CLLocationDistance = 1000;
+        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+        let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+        
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+        
+        let placemark = MKPlacemark(coordinate: coordinates)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "My Work"
+        mapItem.openInMaps(launchOptions: options)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
